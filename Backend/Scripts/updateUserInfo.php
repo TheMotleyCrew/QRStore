5d4fbc7d547108b88c6589625c65c5d923c1007f
bshrinidhi <bshrinidhi1996@gmail.com>
2017-11-19 04:41:36 +0530

    Added email,phone and address to Profile page(Myaccount.html) and some css changes

--- Backend/Scripts/updateUserInfo.php
+++ Backend/Scripts/updateUserInfo.php
@@ -7,10 +7,23 @@ $connect=mysqli_connect('localhost','root','','QRStore');
 		
 		echo $result;
 	}
-	else {
+	else if($toggle==2){
 		$result=mysqli_query($connect,"UPDATE user set password='$password' where username='".$user."'");
 		echo $result;
 	}
+	else if($toggle==3){
+		$result=mysqli_query($connect,"UPDATE user set email='$email' where username='".$user."'");
+		echo $result;
+	}
+	else if($toggle==4){
+		$result=mysqli_query($connect,"UPDATE user set phone='$phone' where username='".$user."'");
+		echo $result;
+	}
+	else if($toggle==5){
+		$result=mysqli_query($connect,"UPDATE user set address='$address' where username='".$user."'");
+		echo $result;
+	}
+
 	
 	
 ?>
